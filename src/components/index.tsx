32e59bb9539a9f341c17fa85c54a54c26a478e97
Changhyeon Yoon <hyeon120411@gmail.com>
2022-03-28 15:28:40 +0900

    eslint, prettier, tsconfig

---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const SampleComponent = () => {
+  return <div>Sample Component</div>
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const SamplePage = () => {
+  return <div>Sample Page</div>;
+};
